4125e7e5e39fd2a2032f772a18e19eebfe16e70c
nani kim <flsk9698@naver.com>
2024-02-20 12:51:44 +0900

    noona api check

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY = `2106e64e536a4ce9aede8c65a5a2a4ee`;
 let news = [];
 const getLatestNews = async() => {
     const url = new URL(
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+        `https://nani-news.netlify.app/top-headlines?page=1&pageSize=5`
     );
     const response = await fetch(url);
     const data = await response.json();
